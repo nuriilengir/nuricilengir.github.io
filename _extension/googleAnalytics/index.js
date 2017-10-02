38d2c3d2f272455c99e80691cdac92cc58dfccb1
Nuri Çilengir <30550129+nuriilengir@users.noreply.github.com>
2017-10-02 23:31:59 +0300

    ga

--- _extension/googleAnalytics/index.js
+++ _extension/googleAnalytics/index.js
@@ -14,6 +14,7 @@ _gaq.push(['_trackPageview']);
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+ ga('require', 'GTM-5HPV8G8');
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 setInterval(function(){_gaq.push(['_trackEvent', 'time_on_site', '5_minutes']);}, 1000 * 60 * 5);
